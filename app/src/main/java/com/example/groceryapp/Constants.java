1de11f586613fedfbbf59f2fc87166527ac24384
nduc99911 <nduc99911@gmail.com>
2021-07-03 23:13:49 +0700

    update add product complete

---
+++ app/src/main/java/com/example/groceryapp/Constants.java
@@ -0,0 +1,20 @@
+package com.example.groceryapp;
+
+public class Constants {
+
+    //product categories
+    public static final String[] productCategori={
+            "Beverages",
+            "Beaty & Personal Care",
+            "Baby Kids",
+            "Breakfast & Daity",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetable",
+            "Orthers"
+
+    };
+}
